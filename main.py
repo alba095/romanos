96d23624fa5b9fa28fcc838a6ba9ac8a6f3d6ea2
Alba Fernandez <albafernandezforero@gmail.com>
2025-12-23 09:42:17 +0100

    Romano a numero

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ unidades= { 1: "I", 2:"II",3:"III",4:"IV",5:"V",6:"VI",7:"VII",8:"VIII",9:"IX"}
 decenas={10:"X",20:"XX",30:"XXX",40:"XL",50:"L",60:"LX",70: "LXX",80:"LXXX",90:"XC"}
 centenas= {100:"C", 200:"CC", 300:"CCC", 400:"CD", 500:"D", 600:"DC", 700:"DCC", 800:"DCCC", 900:"CM"}
 millares={1000: "M", 2000: "MM", 3000: "MMM"}
+valores={"I": 1,"V": 5,"X": 10,"L": 50,"C": 100,"D": 500,"M": 1000}
 
 class RomanNumberError( Exception ):
     pass
@@ -57,41 +58,27 @@ def num_to_roman(number):
             resultado += r
 
     return resultado
-
 print(num_to_roman(1994))
 
+def roman_to_num(roman:str)-> int:
+    total = 0
+
+    for i in range(len(roman)):
+        valor_actual = valores[roman[i]]
+
+        if i + 1 < len(roman):
+            valor_siguiente = valores[roman[i + 1]]
+
+            if valor_actual < valor_siguiente:
+                total -= valor_actual
+            else:
+                total += valor_actual
+        else:
+            total += valor_actual
+
+    return total
+print(roman_to_num("MCMXCIV"))
 
 
 
-
-
-
-
-
-
-
-"""
-#a. 1994 -> MCMXCIV
-def in_to_roman(number):
-    number= list(str(number))#Lo transformamos a cadena y lo descomponemos ["1","9","9","4"]
-    print(number)
-   
-    for i in range(0,len(number)):
-        if i == 0:
-            number[i] = int(number[i]) * 1000
-            values += millares.get(number[i])
-        elif i == 1:
-            number[i] = int(number[i]) * 100
-            values +=centenas.get(number[i])
-        elif i == 2:
-            number[i] = int(number[i]) * 10
-            values +=decenas.get(number[i])
-        elif i == 3:
-            number[i] = int(number[i])
-            values +=unidades.get((number[i]))
-   
-
-    print(values)
-    return values
-    """
 
